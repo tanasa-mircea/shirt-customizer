75705ee6b42565b1c6e1d6d9a7d77fadb64f5904
Mircea Tanasa <tanasamircea@yahoo.com>
2018-11-12 08:30:12 +0200

    update icons config

--- app/routes/customizer.js
+++ app/routes/customizer.js
@@ -5,11 +5,10 @@ import $ from 'jquery';
 export default Route.extend({
   iconsService: service('icons'),
   model() {
-    console.log('this.iconsService ', this.iconsService.initData())
     return Promise.all([
       $.getJSON("/data/shirts.json"),
       $.getJSON("/data/config.json"),
-      // this.iconsService.initData()
+      this.iconsService.initData()
     ])
   }
 });
--- app/services/icons.js
+++ app/services/icons.js
@@ -2,26 +2,22 @@ import Service from '@ember/service';
 import $ from 'jquery';
 
 export default Service.extend({
-  get(icon) {
-    console.log('icon ', icon);
-    console.log('this.icons ', this.icons);
 
-    return icon;
+  get(icon) {
+    return this.icons[icon];
   },
 
   initData() {
     return new Promise(function(resolve, reject) {
       $.getJSON("/data/icons.json")
         .then(function(response) {
-          console.log('this ', response, this)
-          // this.set('icons', response)
-          console.log('response ', response)
+          this.set('icons', response)
           resolve();
         }.bind(this))
         .catch(function(err) {
           reject(err);
         })
-    })
+    }.bind(this))
   }
 
 
