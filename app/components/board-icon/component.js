f90d5290512ff380ceb99121b24a5c24843e37b4
Mircea Tanasa <tanasamircea@yahoo.com>
2018-11-13 16:03:46 +0200

    add icons color change

--- app/components/board-icon/component.js
+++ app/components/board-icon/component.js
@@ -9,9 +9,10 @@ export default Component.extend(DragNDropMixin, {
   classNameBindings: ['class'],
   attributeBindings: ['style'],
   rawStyle: '',
-  style: computed('rawStyle', function() {
-    return new htmlSafe(this.rawStyle);
+  style: computed('rawStyle', 'color', function() {
+    return new htmlSafe(this.rawStyle + `fill: ${ this.color };`);
   }),
+  color: '#000',
   class: 'board-icon',
   iconsService: service('icons'),
   sanitizedIcon: computed('icon', function() {
@@ -27,12 +28,25 @@ export default Component.extend(DragNDropMixin, {
   },
 
   mouseMoveOverride: function(event) {
+    this.set('hasMoved', true);
     this.set('rawStyle', `top: ${ event.y - this.parentOffsets.top }px; left: ${ event.x - this.parentOffsets.left}px;`);
   },
 
   mouseUpOverride: function(event) {
-    this.set('position.x', event.x);
-    this.set('position.y', event.y);
+    if (this.hasMoved) {
+      this.set('position.x', event.x);
+      this.set('position.y', event.y);
+      this.set('hasMoved', false);
+      return;
+    }
+
+
+    this.selected(this);
+  },
+
+  changeColor: function(color) {
+    console.log('change color ', color)
+    this.set('color', color);
   }
 
 
--- app/components/drawing-board/component.js
+++ app/components/drawing-board/component.js
@@ -1,12 +1,24 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+import { computed, observer } from '@ember/object';
 
 export default Component.extend({
   boardService: service('board'),
   icons: computed('boardService.icons', function() {
     return this.boardService.icons;
   }),
+
+  selectedItem: null,
+  shirtColor: '#f00',
+
+  colorChanged: observer('color', function() {
+    if (this.selectedItem) {
+      this.selectedItem.changeColor(this.color);
+    } else {
+      this.set('shirtColor', this.color);
+    }
+  }),
+
 
   init() {
     this._super();
@@ -17,6 +29,13 @@ export default Component.extend({
     this.parentOffsets = {
       left: this.element.offsetLeft,
       top: this.element.offsetTop
+    }
+  },
+
+  actions: {
+    iconSelected: function(icon) {
+      console.log('icon selected ', icon)
+      this.selectedItem = icon;
     }
   }
 });
