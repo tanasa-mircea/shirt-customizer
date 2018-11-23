841836885972b14d10b6ed57d17772788d7bca05
Mircea Tanasa <tanasamircea@yahoo.com>
2018-11-23 11:18:09 +0200

    update items-gallery nav

--- app/components/items-gallery/component.js
+++ app/components/items-gallery/component.js
@@ -6,22 +6,29 @@ import { computed } from "@ember/object";
 export default Component.extend({
   classNameBindings: ["class"],
   page: 1,
-  displayedPage: computed("page", function() {
-    return this.page;
-  }),
+  itemsOnPage: 20,
 
   willRender() {
     this.set("class", "items-gallery");
     this.set("galleryItemStyle", new HtmlSafe("width: " + 10 + "%;"));
   },
 
+  leftDisabled: computed("page", function() {
+    return this.page <= 1;
+  }),
+
+  rightDisabled: computed("page", function() {
+    return this.page * this.itemsOnPage >= this.options.icons.length;
+  }),
+
   actions: {
     leftClickHandler: function() {
-      this.decrementProperty("page");
+      this.set("page", Math.max(1, this.page - 1));
     },
 
     rightClickHandler: function() {
       this.incrementProperty("page");
+      this.set("page", Math.min(2, this.page + 1));
     },
 
     iconDropped: function(icon) {
