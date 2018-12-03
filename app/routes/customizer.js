5aff18fca987f4a8c25dd14e295381d3d22860f8
Mircea Tanasa <tanasamircea@yahoo.com>
2018-12-03 09:56:38 +0200

    add model changes

--- app/routes/customizer.js
+++ app/routes/customizer.js
@@ -1,11 +1,7 @@
 import Route from "@ember/routing/route";
-import {
-  inject as service
-} from "@ember/service";
 import $ from "jquery";
 
 export default Route.extend({
-  iconsService: service("icons"),
   model() {
     return Promise.all([
       $.getJSON("/data/shirts.json"),
---
+++ tests/unit/routes/customizer-test.js
@@ -0,0 +1,35 @@
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
+
+module("Unit | Route | customizer", function(hooks) {
+  setupTest(hooks);
+
+
+
+  test("it exists", function(assert) {
+    var route = this.owner.lookup("route:customizer");
+    assert.ok(route);
+  });
+
+  test("model", function(assert) {
+    var done = assert.async();
+    var route = this.owner.lookup("route:customizer");
+    var results = [];
+    var modelCallback = function() {
+      assert.ok(results.length === 2, "The results contains two sets");
+      assert.ok(results[0] && Object.keys(results[0]).length > 0, "The shirts results contains at least one svg");
+      assert.ok(results[1] && results[1].options && results[1].options.length > 0, "The config results contains at least one option");
+
+      done();
+    };
+
+    route.model()
+      .then(function(res) {
+        results = res;
+        modelCallback();
+      })
+      .catch(function() {
+        modelCallback();
+      });
+  });
+});
