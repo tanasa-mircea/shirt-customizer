74bad199fc1a10b54050d5e36943f61e42698135
Mircea Tanasa <tanasamircea@yahoo.com>
2018-12-04 09:55:08 +0200

    add icon service

--- tests/unit/services/icons-test.js
+++ tests/unit/services/icons-test.js
@@ -4,9 +4,25 @@ import { setupTest } from "ember-qunit";
 module("Unit | Service | icons", function(hooks) {
   setupTest(hooks);
 
+  const mockIcons = {
+    "alien": "alienIcon.jpg"
+  };
+
   // Replace this with your real tests.
-  test("it exists", function(assert) {
+  test("Icons array update", function(assert) {
     let service = this.owner.lookup("service:icons");
-    assert.ok(service);
+
+    service.updateIcons(mockIcons);
+
+    assert.deepEqual(service.icons, mockIcons, "The service icons property should be the same with the provided one");
+  });
+
+  test("Icon get", function(assert) {
+    let service = this.owner.lookup("service:icons");
+    service.updateIcons(mockIcons);
+
+    let resultIcon = service.get("alien");
+
+    assert.equal(resultIcon, mockIcons["alien"], "The requested icon is the same with the one from provided object");
   });
 });
